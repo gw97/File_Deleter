669ea625f9dd455145df6384e6768ccef5622bdd
George Wood <gdaw97@gmail.com>
2018-07-28 21:15:40 +0100

    see if this works

--- delete_files.py
+++ delete_files.py
@@ -1,7 +1,8 @@
-#will delete any file containing the substring specified in the current working directory of the program
 import os
-def delcontains():
-    a = input('File contains:')
+
+def dccwd():
+    #will delete any file containing the substring specified in the current working directory of the program
+    a = input('Files contain:')
     dir_name = os.getcwd()
     test = os.listdir(dir_name)
     for i in test:
@@ -9,3 +10,13 @@ def delcontains():
             os.remove(os.path.join(dir_name, i))
 
 
+
+def dcswd():
+    #deletes files containing a certain substring (as specified), in the directory of users choice
+    dir_name = input('Specify Directory:')
+    a = input('Files contain:')
+    dir_name = os.getcwd()
+    test = os.listdir(dir_name)
+    for i in test:
+        if i.find(a) != -1:
+            os.remove(os.path.join(dir_name, i))
