4d05a669fd940a933c3ec9be064bc23c98d3ef3f
George Wood <gdaw97@gmail.com>
2018-07-28 17:18:59 +0100

    stuff

--- delete_files.py
+++ delete_files.py
@@ -1,10 +1,11 @@
 #will delete any file containing the substring specified in the current working directory of the program
 import os
-def delcontains(string):
+def delcontains():
+    a = input('File contains:')
     dir_name = os.getcwd()
     test = os.listdir(dir_name)
-    for item in test:
-        if item.find(string) != -1:
-            os.remove(os.path.join(dir_name, item))
+    for i in test:
+        if i.find(a) != -1:
+            os.remove(os.path.join(dir_name, i))
 
-delcontains(input("Delete if file contains:"))
+
